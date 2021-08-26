7a934f3eda044cab7b5392d6690bdc05915de619
Aryan Maurya <aryanmaurya1@outlook.com>
2021-08-26 23:38:02 +0530

    Added pipe function support in 'DivideAndWrite' function configuration, tested it. Also changed the position of logging.

--- internal/divmer/divide.go
+++ internal/divmer/divide.go
@@ -11,12 +11,13 @@ import (
 // created them inside a single datatype because didn't wanted to pass
 // 5 arguments to the function.
 type DivideAndWriteConfig struct {
-	Data          []byte   `json:"data"`          // Complete data
-	Metadata      []byte   `json:"metadata"`      // Metadata slice to be prepended before writing slice to file
-	BufferSize    int      `json:"bufferSize"`    // Size of single file
-	BaseDirectory string   `json:"baseDirectory"` //
-	FileName      string   `json:"filename"`
-	OutputRef     *os.File `json:"-"`
+	Data          []byte              `json:"data"`          // Complete data
+	Metadata      []byte              `json:"metadata"`      // Metadata slice to be prepended before writing slice to file
+	BufferSize    int                 `json:"bufferSize"`    // Size of single file
+	BaseDirectory string              `json:"baseDirectory"` // Base directory inside which to create folder to store broken files
+	FileName      string              `json:"filename"`      // starting prefix of all broken files
+	OutputRef     *os.File            `json:"-"`             // Output file reference for writing logs
+	PipeFn        func([]byte) []byte `json:"-"`             // Function through which ever buffer will be passed before writing to file
 }
 
 func checkError(err error) {
@@ -30,11 +31,18 @@ func DivideAndWrite(config DivideAndWriteConfig) int {
 	folderPath := path.Join(config.BaseDirectory, (config.FileName + ".dir"))
 	size := len(config.Data)
 	singleFileSize := config.BufferSize
+
+	// setting default values of configuration if not provided
 	if config.Metadata == nil {
 		config.Metadata = []byte{}
 	}
 	if config.OutputRef == nil {
 		config.OutputRef = os.Stdout
+	}
+	if config.PipeFn == nil {
+		config.PipeFn = func(b []byte) []byte {
+			return b
+		}
 	}
 
 	os.Mkdir(folderPath, os.FileMode(0777))
@@ -50,15 +58,17 @@ func DivideAndWrite(config DivideAndWriteConfig) int {
 
 		singleFilename := fmt.Sprintf("%s_%d", config.FileName, count)
 
+		file, err := os.Create(path.Join(folderPath, singleFilename))
+		singleChunk = append(config.Metadata, singleChunk...)
+		singleChunk = config.PipeFn(singleChunk)
+		checkError(err)
+		file.Write(singleChunk)
+		file.Close()
+
 		// --- Writting logs ---
 		fmt.Fprint(config.OutputRef, "FILE : ", path.Join(folderPath, singleFilename))
 		fmt.Fprintf(config.OutputRef, " || WRITTEN RANGE : %10d %10d \n", i, i+singleFileSize)
 
-		file, err := os.Create(path.Join(folderPath, singleFilename))
-		singleChunk = append(config.Metadata, singleChunk...)
-		checkError(err)
-		file.Write(singleChunk)
-		file.Close()
 		i = i + singleFileSize
 		count++
 	}
