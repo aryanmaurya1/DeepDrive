982217247930b62ec80404475752735bf566f2d9
Aryan Maurya <aryanmaurya1@outlook.com>
2021-09-04 00:39:43 +0530

    Changed function signature of all functions in core/io.go, changed logic accordingly.

--- internal/core/io.go
+++ internal/core/io.go
@@ -1,39 +1,52 @@
 package core
 
 import (
-	"log"
+	"fmt"
 	"os"
 )
 
 // THIS FILE ONLY CONTAINS IO FUNCTIONS WHICH MAY PRODUCE SIDE EFFECTS
 
-func ReadFile(filepath string) []byte {
+func ReadFile(filepath string) ([]byte, error) {
 	file, err := os.OpenFile(filepath, os.O_RDONLY, os.FileMode(1))
-	checkError(err)
+	if err != nil {
+		return nil, err
+	}
 	defer file.Close()
+
 	fileStat, err := file.Stat()
-	checkError(err)
+	if err != nil {
+		return nil, err
+	}
 	fileSize := fileStat.Size()
 
 	buffer := make([]byte, fileSize)
 	n, err := file.Read(buffer)
-	checkError(err)
-	if n != int(fileSize) {
-		log.Fatalf("Error in reading complete data, total len : %d Read len : %d", fileSize, n)
+	if err != nil {
+		return nil, err
 	}
-	return buffer
+
+	if n != int(fileSize) {
+		return nil, fmt.Errorf("error in reading complete data, total len : %d Read len : %d", fileSize, n)
+	}
+	return buffer, nil
 }
 
-func WriteToFile(data []byte, filepath string) {
+func WriteToFile(data []byte, filepath string) error {
 	file, err := os.Create(filepath)
-	checkError(err)
+	if err != nil {
+		return err
+	}
 	defer file.Close()
 
 	n, err := file.Write(data)
-	checkError(err)
-
-	if n != len(data) {
-		log.Fatalf("Error in writing complete data, total len : %d Written len : %d", len(data), n)
+	if err != nil {
+		return err
 	}
 
+	if n != len(data) {
+		return fmt.Errorf("error in writing complete data, total len : %d Written len : %d", len(data), n)
+	}
+
+	return nil
 }
