684887c5972091bd11914b65dfdaeeeaf3bfc49a
Aryan Maurya <aryanmaurya1@outlook.com>
2021-09-04 00:41:19 +0530

    Made custom error structs to send as response in server/utils.go

--- internal/server/utils.go
+++ internal/server/utils.go
@@ -2,16 +2,30 @@ package server
 
 import (
 	"log"
+	"net/http"
 	"os"
 )
+
+type CustomError struct {
+	Result string `json:"result"`
+	Msg    string `json:"msg"`
+}
+
+type SuccessResponse struct {
+	Result string `json:"result"`
+	Msg    string `json:"msg"`
+}
 
 // SetOutputRef : Setting custom logging files.
 func SetOutputRef(o *os.File) {
 	OutputRef = o
 }
 
-func checkError(err error) {
+func checkError(err error, w http.ResponseWriter) *CustomError {
+	var customErr *CustomError = nil
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Println(err.Error())
+		customErr = &CustomError{Result: "fail", Msg: err.Error()}
 	}
+	return customErr
 }
