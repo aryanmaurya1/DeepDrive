0a95e281fd6042979fba826c395c9b7eedeae46b
Aryan Maurya <aryanmaurya1@outlook.com>
2021-09-04 19:58:44 +0000

    Added FileStage type in core/types.go for storing the data regarding processing stage of a particular file.

--- internal/core/types.go
+++ internal/core/types.go
@@ -30,3 +30,19 @@ type FileChunk struct {
 	Index         int    `json:"index"`
 	NodeIds       []int  `json:"node_ids"` // NodeId of all the nodes which have this piece
 }
+
+// FileStages : Store the current state of file i.e what is currently happening with file.
+type FileStages struct {
+
+	// POSSIBLE STAGES
+	// 	1. BREAKING (file is being broken)
+	//  2. DISTRIBUTION (file is being distributed)
+	//	3. INNETWORK (file is inside storage nodes)
+	//	4. RECOLLECTION (file is being collected from network)
+	//	5. MERGING (file is being merged)
+
+	FileID         string // ID of the particular file in processing
+	Stage          string // Current stage of the file
+	PiecesComplete int    // Number of pieces which have passed a particular stage. Resets when stage changes.
+	TotalPieces    int    // Total number of pieces a file is broken into.
+}
