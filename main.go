424d042b10e3b4595ba98915876093e6e5fd94d5
Aryan Maurya <aryanmaurya1@outlook.com>
2021-09-05 23:50:05 +0530

    Added modes in db/models.go file, updated gitignore file. Added statement to migrate models in main.go

--- main.go
+++ main.go
@@ -15,12 +15,15 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	conn, err := db.GetConnection()
+
+	dbConn, err := db.GetConnection()
 	if err != nil {
 		log.Println("Connection to db failed !!")
 	}
-	_ = conn
+	dbConn.AutoMigrate(&db.User{}, &db.File{}, &db.Chunk{})
+
 	r := server.InitRoutes()
 	r.Use(server.Logger) // Attaching the middleware
 	http.ListenAndServe("0.0.0.0:8080", r)
+
 }
