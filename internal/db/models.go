f93008e235e0f91288784a6a7491e916554dbf28
Aryan Maurya <aryanmaurya1@outlook.com>
2021-09-06 23:11:38 +0530

    Changed UserID datatype from string to uint64 in File model. Added testing scratches for testing db topper.

--- internal/db/models.go
+++ internal/db/models.go
@@ -18,7 +18,7 @@ type File struct {
 	ID           uint64    `gorm:"primary_key;auto_increment" json:"id"`
 	CreatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
-	UserID       string    `json:"user_id"`
+	UserID       uint64    `json:"user_id"`
 	User         User
 	OriginalName string `json:"original_name"`
 	Hash         string `json:"hash"`
